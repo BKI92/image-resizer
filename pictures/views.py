0187bb89ce52bcdb1ac9b3848f2ced602ec29b75
Konstantin <balashov-92@mail.ru>
2021-02-13 20:17:02 +0300

    add view to change image size if data is correct

--- pictures/views.py
+++ pictures/views.py
@@ -1,9 +1,7 @@
-from datetime import datetime as dt
+from django.shortcuts import get_object_or_404, redirect, render
 
-from django.shortcuts import render, redirect
-from requests import get
-
-from pictures.forms import PictureForm
+from pictures.forms import PictureForm, ResizeForm
+from pictures.handlers import get_image, get_image_size, resize_image
 from pictures.models import Picture
 
 
@@ -18,21 +16,29 @@ def index(request):
 
 def new(request):
     form = PictureForm(request.POST or None, files=request.FILES or None)
-    if request.method == "POST":
-        if form.is_valid():
-            post = form.save(commit=False)
-            if post.link:
-                content = get(post.link).content
-                date = dt.now()
-                filename = f'{date}.jpg'
-                with open(f'media/images/{filename}', mode='wb') as file:
-                    file.write(content)
-                post.image = filename
-            post.save()
-            return redirect('index')
+    if form.is_valid():
+        picture = form.save(commit=False)
+        if picture.link:
+            picture.image = get_image(picture.link)
+        path = picture.image
+        picture.width, picture.height = get_image_size(path)
+        picture.save()
+        return redirect('index')
     return render(request, 'new.html', {'form': form})
 
 
-def change_image(request, image_id):
-    pass
-
+def change_image(request, picture_id):
+    picture = get_object_or_404(Picture, id=picture_id)
+    form = ResizeForm(request.POST or None, files=request.FILES or None,
+                      instance=picture, initial={'picture_id': picture_id})
+    if form.is_valid():
+        changed_picture = form.save(commit=False)
+        picture.pk = None
+        picture.save()
+        picture.width = changed_picture.width
+        picture.height = changed_picture.height
+        resize_image(picture.image, changed_picture.width,
+                     changed_picture.height)
+        form.save()
+    return render(request, 'resize.html', {'form': form,
+                                           'picture': picture})
